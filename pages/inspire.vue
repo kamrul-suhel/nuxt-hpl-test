4939be8846a43893002d7a6a1694e241e87887f3
kamrul <ka@ontoast.com>
2021-01-06 11:01:35 +0000

    initial

---
+++ pages/inspire.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+
+      {{ name }}
+
+      <v-btn @click="handleFetchData">Fetch data</v-btn>
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    data(){
+      return {
+        name: 'kamrul',
+        age: '20'
+      }
+    },
+    created() {
+    },
+
+    methods:{
+      async handleFetchData(){
+        const response = await axios.post('https://vue.hplmotors.co.uk/api/v1')
+        console.log('response is: ', response.data)
+      }
+    }
+  }
+</script>
